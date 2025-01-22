52cc53eaddc9faf6b1ff5f5cd1af272a54925267
Fredopradana <fredopradana72@gmail.com>
2025-01-22 14:00:21 +0700

    Add files via upload
    Signed-off-by: Fredopradana <fredopradana72@gmail.com>

---
+++ F.UTS Semester 1/menus.js
@@ -0,0 +1,18 @@
+let menuItems = [
+  {Text: 'Home', url: '/'},
+  {Text: 'About', url: '/about.html'},
+  {Text: 'Team', url: '/team.html'},
+  {Text: 'Blog', url: '/blog.html'},
+  {Text: 'Course', url: '/course.html'},
+  {Text: 'Portofolio', url: '/portofolio.html'},
+]
+let menu = document.createElement('ul');
+menuItems.forEach(item => {
+  let menuItems = document.createElement('li');
+  let link = document.createElement('a');
+  link.href = item.url;
+  link.innerText = item.Text;
+  menuItems.appendChild(link);
+  menu.appendChild(menuItems);
+})
+document.body.appendChild(menu);
